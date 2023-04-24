55b70f8703b621359ae657bc35501b9da3cdc0af
Chavey Villegas <chaveyvillegas@Chaveys-MacBook-Pro.local>
2023-04-24 12:38:57 -0700

    added new tests for server.py. 13/13 passed

--- Back-End/test_server.py
+++ Back-End/test_server.py
@@ -1,9 +1,13 @@
 import server
 from server import app
 import unittest
-
+import json
 
 class TestServer(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
 
     # Test get user session key: Passed
     def test_get_user_session_key(self):
@@ -118,6 +122,113 @@ class TestServer(unittest.TestCase):
             self.assertEqual(response.status_code, 401)
             self.assertEqual(response.json['message'], 'Session key is missing')
 
+    # Test generate session key: Passed
+    def test_generate_session_key_length(self):
+        key = server.generate_session_key()
+        self.assertEqual(len(key), 32)
+
+    # Test uniqueness of key: Passed
+    def test_generate_session_key_uniquenss(self):
+        key1 = server.generate_session_key()
+        key2 = server.generate_session_key()
+        self.assertNotEqual(key1, key2)
+
+    # Test login with correct credentials: Pass
+    def test_login_with_correct_credentials(self):
+        # Set up the test client and request body
+        with app.test_client() as client:
+            request_body = {'username': 'adminFaculty', 'password': 'admin'}
+
+            # Send a POST request to the server
+            response = client.post('/login', json=request_body)
+            
+            # Get the JSON content of the response
+            json_response = response.get_json()
+
+            # Check that the server responded with a 200 status code and the expected response body
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json['userName'], 'userStudent')
+            self.assertEqual(response.json['permission'], 'faculty')
+
+    # Test caching of session key: Passed
+    def test_caching_of_session_key(self):
+        # Generate a test session key for a test user
+        user_id = 123
+        session_key = server.generate_test_session_key(user_id)
+
+        # Set up the test client and request headers
+        with app.test_client() as client:
+            headers = {'Authorization': session_key}
+
+            # Send a PUT request to the server to ensure the session key is cached
+            response = client.put('/', headers=headers)
+
+            # Check that the server responded with a 200 status code and "Put complete" message
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data.decode('utf-8'), 'Put complete')
+
+            # Check that the session key is in the cache
+            self.assertEqual(server.cache.get(session_key), user_id)
+
+            # Generate a new session key for the same user
+            new_session_key = server.generate_test_session_key(user_id)
+
+            # Check that the new session key is not equal to the old session key
+            self.assertNotEqual(new_session_key, session_key)
+
+            # Check that the new session key is in the cache and has the same user_id
+            self.assertEqual(server.cache.get(new_session_key), user_id)
+
+    # Test query with valid session key: Pass
+    def test_query_with_valid_login(self):
+        with app.test_client() as client:
+            request_body = {'username': 'adminFaculty', 'password': 'admin'}
+
+            # Send a POST request to the server
+            login_response = client.post('/login', json=request_body)
+            sessionId = login_response.json['sessionId']
+            reportName = 'courseInfo'
+            reportFilters = 'allCoursesAllDepartments'
+            department = 'Computer Science'
+            class_name = 'Software Engineering'
+            headers = {'Content-Type': 'application/json'}
+
+            response = client.post('/query',
+                       headers=headers,
+                       json={'sessionId': sessionId,
+                             'reportName': reportName,
+                             'reportFilters': reportFilters,
+                             'departments': department,
+                             'class': class_name})
+            
+            #Test for correct response
+            self.assertAlmostEqual(response.json['message'], 'Json was correctly validated')
+
+    # Test query with invalid session key: Pass
+    def test_query_with_invalid_session_key(self):
+        with app.test_client() as client:
+            request_body = {'username': 'adminFaculty', 'password': 'admin'}
+
+            # Send a POST request to the server
+            login_response = client.post('/login', json=request_body)
+            sessionId = 'invalid_session_id'
+            reportName = 'courseInfo'
+            reportFilters = 'allCoursesAllDepartments'
+            department = 'Computer Science'
+            class_name = 'Software Engineering'
+            headers = {'Content-Type': 'application/json'}
+
+            response = client.post('/query',
+                       headers=headers,
+                       json={'sessionId': sessionId,
+                             'reportName': reportName,
+                             'reportFilters': reportFilters,
+                             'departments': department,
+                             'class': class_name})
+            
+            # Test for correct response
+            self.assertEqual(response.json['message'], 'Invalid session key')
+            
 if __name__ == '__main__':
     unittest.main()
 
