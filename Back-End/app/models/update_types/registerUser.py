94c3811c0475ed194d022cec34fb9e4bb90feac3
jakerjohnson21 <jakekjohnson@outlook.com>
2023-05-03 17:07:55 -0700

    b

--- Back-End/app/models/update_types/registerUser.py
+++ Back-End/app/models/update_types/registerUser.py
@@ -40,23 +40,21 @@ class RegisterUser:
             if Users.query.filter_by(username=username).first() is not None:
                 return jsonify({"error": "There is already a user with the username {}".format(username)})
             else:
-                match permission:
-                    case("admin"):
-                        return Users.create(username, name, password, job_title, permission)
-                    case("student"):
-                        phone_number = request_json["phoneNumber"]
-                        address = request_json["address"]
-                        date_of_birth = request_json["dateOfBirth"]
-                        major = request_json["major"]
-                        minor = request_json["minor"]
-                        new_student = Student.create(name, phone_number, address, date_of_birth, major, minor)
+                if permission == "admin":
+                    return Users.create(username, name, password, job_title, permission)
+                elif permission == "student":
+                    phone_number = request_json["phoneNumber"]
+                    address = request_json["address"]
+                    date_of_birth = request_json["dateOfBirth"]
+                    major = request_json["major"]
+                    minor = request_json["minor"]
+                    new_student = Student.create(name, phone_number, address, date_of_birth, major, minor)
 
-                        # program with the same name and grab id
-                        program_name = '%' + major + '%'
-                        program = db.session.query(Programs.id).filter(Programs.name.like(program_name)).first()
-                        faculty = Faculty.query.first()
-                        ProgramOutline.create(new_student.id, program[0], faculty.id)
-                        return Users.create_from_id(new_student.id, username, password)
-                    case("faculty"):
-                        return Faculty.create()
-                return
+                    # program with the same name and grab id
+                    program_name = '%' + major + '%'
+                    program = db.session.query(Programs.id).filter(Programs.name.like(program_name)).first()
+                    faculty = Faculty.query.first()
+                    ProgramOutline.create(new_student.id, program[0], faculty.id)
+                    return Users.create_from_id(new_student.id, username, password)
+                else:
+                    return
